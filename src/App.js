63fde25ee97460615ef7a94e37d4a188d889b5ea
AlexEMartin <alexeusebiomartin@gmail.com>
2022-04-11 10:10:24 -0300

    ricky and morty fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components'
+
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const Container = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  margin: 2rem;
+
+`;
+
+const Title = styled.h1`
+  width: 100%;
+  font-size: 45px;
+  font-weight: 700;
+  font-family: 'Grape Nuts', cursive;
+`;
+
+const Desc = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const Image = styled.img`
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px;
+`;
 
 function App() {
+
+  const [ personajes, setPersonajes ] = useState([])
+
+  useEffect(() => {
+    obtenerDatos()
+  }, [])
+
+  const obtenerDatos = async() => {
+    const resp = await fetch('https://rickandmortyapi.com/api/character/')
+    const data = await resp.json()
+    setPersonajes(data.results)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page className="App">
+        <Title>Ricky and Morty</Title>
+        {
+          personajes
+          .filter(p => p.id !== 19)
+          .map(p => (
+            <Container key={p.id}>
+              <Image src={p.image} alt='personaje' />
+              <h3>Name: {p.name}</h3>  
+              <Desc>Status: {p.status}</Desc>
+              <Desc>Species: {p.species}</Desc>
+            </Container>
+          ))
+        }
+    </Page>
   );
 }
 
